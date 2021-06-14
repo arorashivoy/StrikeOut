068d5d7edaf08af934bcf6274a1015df620051e1
arorashivoy <shivoy1183@gmail.com>
2021-06-14 18:21:40 +0530

    adding Project Files

---
+++ CheckLists/CheckListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckListsApp.swift
+//  CheckLists
+//
+//  Created by Shivoy Arora on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
